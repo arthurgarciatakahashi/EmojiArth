41487024a4e62aadf7c0d85638b421c2da2556bb
Arthur Garcia Takahashi <arthurgt@alumni.usp.br>
2021-03-22 16:29:06 -0300

    first commit

---
+++ EmojiArt/EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by arthur takahashi on 22/03/21.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EmojiArt/EmojiArt/EmojiArtDocumentView.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+struct EmojiArtDocumentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
