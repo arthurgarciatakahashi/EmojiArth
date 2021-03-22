ef01bbd5673732b22265158c66d1338d8a7e92b5
Arthur Garcia Takahashi <arthurgt@alumni.usp.br>
2021-03-22 19:03:57 -0300

    add stanford extension file

--- EmojiArt/EmojiArt/EmojiArt.swift
+++ EmojiArt/EmojiArt/EmojiArt.swift
@@ -3,11 +3,27 @@ import SwiftUI
 struct EmojiArt {
     var backgroundURL: URL?
     var emojis = [Emoji]()
+    private var uniqueEmojiId = 0
     
-    struct Emoji {
+    struct Emoji: Identifiable {
+        let id: Int
         let text: String
         var x: Int
         var y: Int
         var size: Int
+        
+        fileprivate init(id: Int, text: String, x: Int, y: Int, size: Int) {
+            self.id = id
+            self.text = text
+            self.x = x
+            self.y = y
+            self.size = size
+        }
     }
+    
+    mutating func addEmoji(_ text: String, x: Int, y: Int, size: Int) {
+        uniqueEmojiId += 1
+        self.emojis.append(Emoji(id: uniqueEmojiId, text: text, x: x, y: y, size: size))
+    }
+    
 }
--- EmojiArt/EmojiArt/EmojiArtDocument.swift
+++ EmojiArt/EmojiArt/EmojiArtDocument.swift
@@ -2,4 +2,27 @@ import SwiftUI
 
 class EmojiArtDocument: ObservableObject {
     static let palette: String = "🏰🌋🌙🐏🐓🐉🔥💧⚡️🌪💥🦂🐍🦖🦉🧙🏻‍♂️🧝🏻‍♀️🧝🏻‍♂️🧝🏻🧟‍♂️"
+    @Published private var emojiArt: EmojiArt = EmojiArt()
+    
+    // MARK: - Intent
+    
+    func addEmoji(_ emoji: String, at location: CGPoint, size: CGFloat) {
+        emojiArt.addEmoji(emoji, x: Int(location.x), y: Int(location.y), size: Int(size))
+    }
+    
+    func moveEmoji(_ emoji: EmojiArt.Emoji, by offset: CGSize) {
+        if let index = emojiArt.emojis.firstIndex(matching: emoji) {
+            emojiArt.emojis[index].x += Int(offset.width)
+            emojiArt.emojis[index].y += Int(offset.height)
+        }
+    }
+    
+    func scaleEmoji(_ emoji: EmojiArt.Emoji, by scale: CGFloat) {
+        if let index = emojiArt.emojis.firstIndex(matching: emoji) {
+            emojiArt.emojis[index].size = Int(
+                (CGFloat(emojiArt.emojis[index].size) * scale).rounded(.toNearestOrEven)
+            )
+        }
+    }
+    
 }
